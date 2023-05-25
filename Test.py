0a7bca19809fd2ba00b498e61b488027a6dcd6df
TheRealWushuMaster <chiossi@gmail.com>
2023-05-25 14:40:55 -0300

    Added integral calculation, need to solve graphics

--- Test.py
+++ Test.py
@@ -3,16 +3,18 @@ import matplotlib.pyplot as plt
 from sympy import symbols, lambdify
 from scipy.integrate import quad
 import custom_functions_lib
+from scipy.integrate import trapz
 
 def plot_math_function(expression, variable, start, end, num_points):
+    global x_vals, y_vals
     x_vals = np.linspace(start, end, num_points)
     y_vals = evaluate_expression(expression, variable, x_vals)
 
-    plt.plot(x_vals, y_vals)
+    plt.plot(x_vals, y_vals, color="red")
     plt.xlabel(variable)
     plt.ylabel('y')
     plt.title('Plot of ' + expression)
-    plt.grid(True)
+    plt.grid(visible=True, linestyle="--")
 
     # Enable interactive mode for selecting range
     plt.gca().set(xlim=(start, end), ylim=(np.min(y_vals), np.max(y_vals)))
@@ -21,6 +23,7 @@ def plot_math_function(expression, variable, start, end, num_points):
 
     # Connect the mouse event to the handler function
     plt.gcf().canvas.mpl_connect('button_press_event', on_click)
+    plt.gca().figure.canvas.draw()
 
     plt.show()
 
@@ -56,38 +59,68 @@ def on_click(event):
     if event.button == 1 and event.inaxes is not None:
         x_start = event.xdata
         print(f"x_start = {x_start}")
-        #plt.gca().set_autoscale_on(True)
-        #plt.gcf().canvas.mpl_disconnect(event)
+        plt.gca().set_autoscale_on(True)
+        plt.gcf().canvas.mpl_disconnect(event)
         plt.gcf().canvas.mpl_connect('motion_notify_event', on_drag)
-        #plt.gcf().canvas.mpl_connect('button_release_event', on_release)
+        plt.gcf().canvas.mpl_connect('button_release_event', on_release)
 
 def on_drag(event):
-    if event.inaxes is not None:
-        x_end = event.xdata
-        print(f"x_end = {x_end}")
-        plt.gca().lines[-1].set_xdata([x_start, x_end])
-        #plt.gca().figure.canvas.draw()
-
-def on_release(event):
+    global x_end
     if event.button == 1:
-        plt.gcf().canvas.mpl_disconnect(event)
-        plt.gcf().canvas.mpl_disconnect('motion_notify_event')
-        plt.gca().lines[-1].set_xdata([x_start, event.xdata])
+        x_end = event.xdata
+        #plt.gca().lines[-1].set_xdata([x_start, x_end])
         plt.gca().figure.canvas.draw()
 
-        x_vals = np.linspace(x_start, event.xdata, 100)
-        y_vals = evaluate_expression(input_expression, variable_name, x_vals)
-        integral_value, _ = quad(evaluate_expression, x_start, event.xdata, args=(variable_name, x_vals))
+        if x_start > x_end:
+            start = x_end
+            end = x_start
+        else:
+            start = x_start
+            end = x_end
+        
+        x_vals_selected = x_vals[(x_vals >= start) & (x_vals <= end)]
+        y_vals_selected = y_vals[(x_vals >= start) & (x_vals <= end)]
+        
+        integral = trapz(y_vals_selected, x_vals_selected)
+        print(f'Integral = {integral}')
 
-        print("Integral value:", integral_value)
+        # Fill the area under the curve
+        plt.fill_between(x_vals_selected, y_vals_selected, alpha=1, color="darkgreen")
 
-        # Highlight the selected range
-        plt.fill_between(x_vals, 0, y_vals, where=(x_vals >= x_start) & (x_vals <= event.xdata), alpha=0.3)
+        plt.draw()
+    # if event.inaxes is not None:
+    #     x_end = event.xdata
+    #     print(f"x_end = {x_end}")
+    #     plt.gca().lines[-1].set_xdata([x_start, x_end])
+    #     #plt.gca().figure.canvas.draw()
 
-        plt.show()
+def on_release(event):
+    
+    # Reset the x_start variable to None
+    x_start = None
+    x_end = None
+    plt.clf()
+    plot_math_function(input_expression, variable_name, start_value, end_value, num_points)
+    
+    # if event.button == 1:
+    #     plt.gcf().canvas.mpl_disconnect(event)
+    #     plt.gcf().canvas.mpl_disconnect('motion_notify_event')
+    #     plt.gca().lines[-1].set_xdata([x_start, event.xdata])
+    #     plt.gca().figure.canvas.draw()
+
+    #     x_vals = np.linspace(x_start, event.xdata, 100)
+    #     y_vals = evaluate_expression(input_expression, variable_name, x_vals)
+    #     integral_value, _ = quad(evaluate_expression, x_start, event.xdata, args=(variable_name, x_vals))
+
+    #     print("Integral value:", integral_value)
+
+    #     # Highlight the selected range
+    #     plt.fill_between(x_vals, 0, y_vals, where=(x_vals >= x_start) & (x_vals <= event.xdata), alpha=0.3)
+
+    #     plt.show()
 
 # Example usage
-input_expression = "normal(x)"
+input_expression = "sin(2*x)/x"
 variable_name = "x"
 start_value = -5
 end_value = 5
