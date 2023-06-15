f8795cfb3cf826450d46bfff2270523f77d1ffc3
TheRealWushuMaster <chiossi@gmail.com>
2023-06-15 17:55:11 -0300

    Corrected classes and added canvas

--- GUI.py
+++ GUI.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 import customtkinter as ctk
 from settings import *
-from define_vars import *
+import define_vars
 
 class App(tk.Tk):
     def __init__(self):
@@ -13,13 +13,11 @@ class App(tk.Tk):
         self.geometry(f"{WINDOW_SIZE[0]}x{WINDOW_SIZE[1]}")
         self.resizable = (True, True)
         self.iconbitmap("Plot.ico")
-        
-        # Widgets
-        self.controls = Controls(self)
+        define_vars.create_variables()
 
-        # Layout
-        #self.columnconfigure
-        
+        # Widgets
+        Controls(self)
+
         # Run
         self.mainloop()
 
@@ -28,35 +26,39 @@ class Controls(ttk.Frame):
         super().__init__(master=parent)
         
         # Create control widgets
-        #frame_function = self.frame()
-        function_label = self.label("Function: y = ")
-        function_entry = self.text_input(text_var=function_text, width=50)
+        function_frame = self.create_function_frame()
+        variable_limits_frame = self.create_variable_limits_frame()
+        points_per_unit_frame = self.create_points_per_unit_frame()
         
         # Place widgets
-        #function_label.pack(side=tk.LEFT, padx=0, pady=DEF_PAD)
-        #function_entry.pack(side=tk.LEFT, padx=0, pady=DEF_PAD)
-        self.create_function_text()
+        function_frame.pack(padx=DEF_PAD, pady=DEF_PAD)
+        variable_limits_frame.pack(padx=DEF_PAD, pady=0)
+        points_per_unit_frame.pack(padx=DEF_PAD, pady=DEF_PAD)
         
-        self.place(x=0, y=0, relwidth=1, height=100)
+        canvas = tk.Canvas(parent, bg="white")
+        self.pack()
+        #self.place(x=0, y=0, relwidth=1, height=130)
+        canvas.pack(fill="both")
 
-    def create_function_text(self):
-        #frame = self.frame()
-        #ttk.Label(text="Test").pack()
-        function_label = self.label("Function: y = ")
-        function_entry = self.text_input(function_text, width=FUNCTION_ENTRY_WIDTH)
-        function_label.pack()
-        function_entry.pack()
-
-    def frame(self):
-        frame = ttk.Frame(master=self)
-        return frame
+    def create_function_frame(self):
+        function_text_frame = ttk.Frame(master=self)
+        ttk.Label(master=function_text_frame, text="Function: y =").pack(side=tk.LEFT)
+        ttk.Entry(master=function_text_frame, textvariable=define_vars.function_text, width=FUNCTION_ENTRY_WIDTH).pack(side=tk.LEFT)
+        return function_text_frame
     
-    def text_input(self, text_var = None, width=20):
-        text_input = ttk.Entry(self, textvariable=text_var, width=width)
-        return text_input
+    def create_variable_limits_frame(self):
+        variable_limits_frame = ttk.Frame(master=self)
+        ttk.Label(master=variable_limits_frame, text=f"Limits of the {define_vars.variable_name.get()} variable").pack(pady=int(DEF_PAD/2))
+        ttk.Label(master=variable_limits_frame, text="Start:").pack(side=tk.LEFT)
+        ttk.Entry(master=variable_limits_frame, textvariable=define_vars.start_value, width=DEFAULT_ENTRY_WIDTH).pack(side=tk.LEFT)
+        ttk.Label(master=variable_limits_frame, text="End:").pack(side=tk.LEFT)
+        ttk.Entry(master=variable_limits_frame, textvariable=define_vars.end_value, width=DEFAULT_ENTRY_WIDTH).pack(side=tk.LEFT)
+        return variable_limits_frame
     
-    def label(self, text):
-        label = ttk.Label(self, text=text)
-        return label
+    def create_points_per_unit_frame(self):
+        points_per_unit_frame = ttk.Frame(master=self)
+        ttk.Label(master=points_per_unit_frame, text="Points per unit:").pack(side=tk.LEFT)
+        ttk.Entry(master=points_per_unit_frame, textvariable=define_vars.num_points, width=DEFAULT_ENTRY_WIDTH).pack(side=tk.LEFT)
+        return points_per_unit_frame
 
 App()
--- define_vars.py
+++ define_vars.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from settings import *
 
 function_text = None
 variable_name = None
@@ -8,9 +9,8 @@ num_points = None
 
 def create_variables():
     global function_text, variable_name, start_value, end_value, num_points
-    function_text = tk.StringVar(value = "sin(2*x)/x")
-    variable_name = tk.StringVar(value = "x")
-    start_value = tk.IntVar(value = -5)
-    end_value = tk.IntVar(value = 5)
-    num_points = tk.IntVar(value = 10)
-    return function_text.get(), variable_name.get(), start_value.get(), end_value.get(), num_points.get()
+    function_text = tk.StringVar(value = DEF_FUNCTION)
+    variable_name = tk.StringVar(value = DEF_VARIABLE)
+    start_value = tk.IntVar(value = DEF_START_VALUE)
+    end_value = tk.IntVar(value = DEF_END_VALUE)
+    num_points = tk.IntVar(value = DEF_PPU)
